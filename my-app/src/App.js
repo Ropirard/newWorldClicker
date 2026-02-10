ecadab9f8ed0855de491d3180fb9af3a2bc00aed
Robin Pirard <pirardrobin745@gmail.com>
2026-02-10 21:54:22 +0100

    Add market + money features

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,26 +5,38 @@ function App() {
   const [mineral, setMineral] = useState(0);
   const [miner, setMiner] = useState(1);
   const [tiredness, setTiredness] = useState(0);
+  const [marketPrice, setMaketPrice] = useState(5)
+  const [money, setMoney] = useState(0)
+
   const [unlockMiner, setUnlockMiner] = useState(false)
 
   useEffect(() => {
     if (mineral >= 50) {
       setUnlockMiner(true);
     }
-  })
+  }, [mineral])
 
-  function randomTiredness(min, max) {
+  useEffect(() => {
+    const marketPriceTimer = setInterval(() => {
+      setMaketPrice(randomFunction(4, 9));
+    }, 9000);
+
+    return () => clearInterval(marketPriceTimer);
+  }, []);
+
+  function randomFunction(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-  const handleDelete = () => {
-    setMineral(mineral - 1)
   }
 
   const handleClick = () => {
     setMineral(mineral + miner)
-    setTiredness(tiredness + randomTiredness(3, 4))
+    setTiredness(tiredness + randomFunction(3, 4))
   };
+
+  const handleSell = () => {
+    setMoney(marketPrice * mineral);
+    setMineral(0)
+  }
 
   const addMiner = () => {
     setMiner(miner + 1)
@@ -32,20 +44,30 @@ function App() {
 
   return (
     <>
+      {/** Data of meterials on our possession */}
       <h1>
         Minerals collected : {mineral}
         <button onClick={handleClick}>Collect</button>
-        <button onClick={handleDelete}>Throw</button>
       </h1>
+      {/** Level of fatigue */}
       <h1>
         Tiredness : {tiredness/10}%
       </h1>
+      {/** if condition is met, we unlock Miner(s) which adds +1 at the material harvest for each miner */}
       {unlockMiner && (
         <h1>
           Miners hired : {miner - 1}
           <button onClick={addMiner}>Hire miner</button>
         </h1>
       )}
+      <h1>
+        Market!
+      </h1>
+        <h2>Price of now : {marketPrice}</h2>
+      <h1>
+        Money : {money}$
+        <button onClick={handleSell}>Sell !</button>
+      </h1>
     </>
   )
   
